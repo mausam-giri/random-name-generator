a747afd1c5d369bea7dbe7268e90f6d8f2bc5799
Mausam Giri <48901104+byte2code@users.noreply.github.com>
2023-01-09 18:50:02 +0000

    changes

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Prefix or Suffix file name with combination'
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setting up
+setup(
+    name="combogen",
+    version=VERSION,
+    author="byte2code (Mausam Giri)",
+    author_email="<mausamkumargiri@proton.me>",
+    url="https://github.com/byte2code/combogen",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['python', 'combination', 'files', 'unique', 'random filename generator', 'unique prefix suffix'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
